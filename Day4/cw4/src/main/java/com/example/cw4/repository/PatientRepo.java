12eae5cf19882fc9eb8fd27da3f5fbf720c27a45
barathsaki2 <151364152+barathsaki2@users.noreply.github.com>
2024-04-22 20:33:54 +0530

    Delete Day4 directory

--- Day4/cw4/src/main/java/com/example/cw4/repository/PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.cw4.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.cw4.model.Patient;
-
-public interface PatientRepo extends JpaRepository<Patient, Integer>{
-    
-}
